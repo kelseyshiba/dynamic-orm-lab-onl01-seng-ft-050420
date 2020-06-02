8121eaeef20861d78e233eaf6de81b4dfb91a120
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-06-02 15:47:23 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -35,8 +35,5 @@ class InteractiveRecord
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
   
-  # def col_names_for_insert
-  # self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  # end
 
 end
