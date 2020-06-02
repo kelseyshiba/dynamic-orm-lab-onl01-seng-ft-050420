1c6ae48e99dd4d3daa96dfcfd91eefd0d69facd2
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-06-02 15:31:24 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  attr_accessor :id
+  attr_accessor :id, :name
   
   def initialize(options={})
     options.each do  |property, value|
