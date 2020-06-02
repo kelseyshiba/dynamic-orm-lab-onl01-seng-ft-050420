c7f0bf542fbc3378867817aef40ae14460a09914
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-06-02 15:52:27 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -42,13 +42,6 @@ class InteractiveRecord
     end
     values.join(", ")
   end
-  
-  def values_for_insert
-  values = []
-  self.class.column_names.each do |col_name|
-    values << "'#{send(col_name)}'" unless send(col_name).nil?
-  end
-  values.join(", ")
-  end
+
 
 end
